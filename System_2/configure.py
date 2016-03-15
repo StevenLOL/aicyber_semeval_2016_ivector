8c391e5205f3213410ab6257827fd99905f091c5
Steven Du <stevenlol@qq.com>
2016-03-15 23:23:42 +0800

    Update configure.py

--- System_2/configure.py
+++ System_2/configure.py
@@ -1,6 +1,5 @@
-FILE_LINE_SENTANCE='./data/2016_aicyber_wiki_weibo_douban_words_l5.txt'
-TRAIN_SET='./data/douban_c5.train.txt'
-EVAL_SET='./data/douban_c5.eval.txt'
+
+#IMDB
 IMDB_BOW_DATA_FOLD= './imdb'
 IMDB_FILE_TEST= './imdb/testData.tsv'
 IMDB_FILE_TEST_LINE_SENTANCE= './tempfolder/imdb_test_line_sentance'
@@ -8,11 +7,13 @@ IMDB_FILE_ALL_LINE_SENTANCE= './tempfolder/imdb_all_train_unlabled_line_sentance
 IMDB_FILE_TRAIN_W2V_LINE_SENTANCE= './tempfolder/imdb_train_line_sentance'
 IMDB_FILE_TRAIN_IVECTOR_LINE_SENTANCE=IMDB_FILE_ALL_LINE_SENTANCE
 IMDB_W2V_MODEL= './tempfolder/imdb_trained_75000.w2v.20.bin'
+
+#SEMEVAL
 SEMEVAL_TRAIN='../semeval2016/100_topics_100_tweets.sentence-three-point.subtask-A.train.gold.txt.full'
 SEMEVAL_DEV='../semeval2016/100_topics_100_tweets.sentence-three-point.subtask-A.dev.gold.txt.full'
 SEMEVAL_DEV_TEST='../semeval2016/100_topics_100_tweets.sentence-three-point.subtask-A.devtest.gold.txt.full'
 SEMEVAL_TRAIN_FILE_LINE_SENTANCE='./tempfolder/semeval.train.2016.txt'
-SEMEVAL_DEV_FILE_LINE_SENTANCE='./tempfolder/semeval.dev,2016.txt'
+SEMEVAL_DEV_FILE_LINE_SENTANCE='./tempfolder/semeval.dev.2016.txt'
 SEMEVAL_DEV_TEST_FILE_LINE_SENTANCE='./tempfolder/semeval.dev.test.2016.txt'
 
 
@@ -25,7 +26,7 @@ KALDI_DATA_SEMEVAL_DEVTEST='./data/semeval_devtest'
 
 
 
-
+#Scoring of Semeval
 SCORE_SCRIPT='../semeval2016/score-semeval2016-task4-subtaskA.pl'
 SCORE_REF_DEV='../semeval2016/100_topics_100_tweets.sentence-three-point.subtask-A.dev.gold.txt.full'
 SCORE_REF_DEVTEST='../semeval2016/100_topics_100_tweets.sentence-three-point.subtask-A.devtest.gold.txt.full'
